6672c034cf6b68992e0a831af856c558fea825db
thenewjapzzz <142543292+thenewjapzzz@users.noreply.github.com>
2023-08-17 18:24:11 -0300

    ex-3.py

--- lista-1/ex-3.py
+++ lista-1/ex-3.py
@@ -2,3 +2,6 @@ dias = int(input('Dias: '))
 horas = int(input('Horas: '))
 minutos = int(input('Minutos: '))
 segundos = int(input('Segundos: '))
+
+total_em_segundos = dias * 24 * 3600 + horas * 3600 + minutos * 60 + segundos
+print (f'Convertido em segundos o total é {total_em_segundos} segundos')
