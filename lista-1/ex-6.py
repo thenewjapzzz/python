3273b9ca60f97244f5c7894099d42f3065a06b1e
thenewjapzzz <142543292+thenewjapzzz@users.noreply.github.com>
2023-08-17 18:33:55 -0300

    ex-6.py

--- lista-1/ex-6.py
+++ lista-1/ex-6.py
@@ -1,3 +1,7 @@
+#Calcule o tempo de uma viagem de carro.
+#Pergunte a distância a percorrer e a velocidade média
+#esperada para a viagem.
+
 distância = float(input('Informe a distância da viagem em quilômetros: '))
 velocidade_média = float(input('Informe a velocidade média esperada: '))
 tempo = distância / velocidade_média
