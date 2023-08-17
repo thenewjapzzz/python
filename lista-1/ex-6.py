3f6a98b810730ec800a4a31493f3243329dd6b2f
thenewjapzzz <142543292+thenewjapzzz@users.noreply.github.com>
2023-08-17 18:25:42 -0300

    ex-6.py

---
+++ lista-1/ex-6.py
@@ -0,0 +1,5 @@
+distância = float(input('Informe a distância da viagem em quilômetros: '))
+velocidade_média = float(input('Informe a velocidade média esperada: '))
+tempo = distância / velocidade_média
+
+print (f'O tempo da viagem é de {tempo} horas')
