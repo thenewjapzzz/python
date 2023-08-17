7c47fd292c75eaa7f8f1c9e9a1c695d9b8ec704f
thenewjapzzz <142543292+thenewjapzzz@users.noreply.github.com>
2023-08-17 18:35:01 -0300

    ex-9.py

--- lista-1/ex-9.py
+++ lista-1/ex-9.py
@@ -1,3 +1,8 @@
+#Escreva um programa que pergunte a quantidade de km percorridos
+#por um carro alugado pelo usuário, assim como a quantidade de dias
+#pelos quais o carro foi alugado. Calcule o preço a pagar,
+#sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por km rodado.
+
 quantidade_percorrida = float(input('Quantos km foram percorridos: '))                  
 dias = int(input('Quantos dias foram o aluguel: '))
 total_pagar = dias * 60 + quantidade_percorrida * 0.15
