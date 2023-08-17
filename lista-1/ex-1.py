5d4e8133f3aab77dcdc7eca3b299b3008c58cfac
thenewjapzzz <142543292+thenewjapzzz@users.noreply.github.com>
2023-08-17 18:28:28 -0300

    ex-1.py

--- lista-1/ex-1.py
+++ lista-1/ex-1.py
@@ -1,5 +1,5 @@
 #Faça um programa que peça dois números
-#e imprima a soma deses
+#e imprima a soma deses dois números
 
 x = int(input('Digite um número: '))
 y = int(input('Digite outro número: '))
