4586f8d829fb8115094bd7e96cd1cb3f682beee3
thenewjapzzz <142543292+thenewjapzzz@users.noreply.github.com>
2023-08-17 18:33:16 -0300

    ex-4.py

--- lista-1/ex-4.py
+++ lista-1/ex-4.py
@@ -1,3 +1,7 @@
+#Faça um programa que calcule o aumento de um salário.
+#Ele deve solicitar o valor do salário e a porcentagem
+#do aumento. Exiba o valor do aumento e do novo salário.
+
 salário = float(input('Digite seu salário: '))
 porcentagem = float(input('Digite um aumento: '))
 novo_salário = salário + salário * porcentagem / 100
