7d886a86fe3733cea6545b6bdd1a0a359c9257ac
thenewjapzzz <142543292+thenewjapzzz@users.noreply.github.com>
2023-08-18 22:53:29 -0300

    ex-5.py

---
+++ lista-3/ex-5.py
@@ -0,0 +1,9 @@
+#Dados dois números inteiros positivos, determinar o máximo divisor comum entre eles usando
+#o algoritmo de Euclides.
+
+a = int(input('Número a: '))
+b = int(input('Número b: '))
+while a % b != 0:
+    a , b = b , a % b
+
+print(f'mdc = {b}')
