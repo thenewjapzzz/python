d016b3437f4308d6e435d8755c1425d925fb1779
thenewjapzzz <142543292+thenewjapzzz@users.noreply.github.com>
2023-08-18 22:32:15 -0300

    ex-1.py

--- lista-3/ex-1.py
+++ lista-3/ex-1.py
@@ -1,1 +1,6 @@
-
+nota = float(input('Uma nota de zero a dez: '))
+while (nota > 10) or (nota < 0):
+    print('Tente novamente ')
+    nota = float(input('Uma nota de zero a dez: '))
+if (nota <= 10) or (nota >= 0):
+    print('Nota válida')
