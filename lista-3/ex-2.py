7dc76071379ff0d387c16a7755a6336106d934da
thenewjapzzz <142543292+thenewjapzzz@users.noreply.github.com>
2023-08-18 22:55:00 -0300

    ex-2.py

--- lista-3/ex-2.py
+++ lista-3/ex-2.py
@@ -1,3 +1,7 @@
+#Faça um programa que leia um nome de
+#usuário e a sua senha e não aceite a senha igual ao
+#nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+
 nome = str(input('Nome de usuário: '))
 senha = str(input('Senha: '))
 while nome == senha:
