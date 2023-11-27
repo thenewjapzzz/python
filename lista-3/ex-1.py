971f4637d0d3d652166fb9be2b8999b7bad4ccc3
thenewjapzzz <142543292+thenewjapzzz@users.noreply.github.com>
2023-11-27 09:40:16 -0300

    Delete lista-3 directory

--- lista-3/ex-1.py
+++
@@ -1,9 +0,0 @@
-#Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor
-#seja inválido e continue pedindo até que o usuário informe um valor válido.
-
-nota = float(input('Uma nota de zero a dez: '))
-while (nota > 10) or (nota < 0):
-    print('Tente novamente ')
-    nota = float(input('Uma nota de zero a dez: '))
-if (nota <= 10) or (nota >= 0):
-    print('Nota válida')
--- lista-3/ex-5.py
+++
@@ -1,9 +0,0 @@
-#Dados dois números inteiros positivos, determinar o máximo divisor comum entre eles usando
-#o algoritmo de Euclides.
-
-a = int(input('Número a: '))
-b = int(input('Número b: '))
-while a % b != 0:
-    a , b = b , a % b
-
-print(f'mdc = {b}')
